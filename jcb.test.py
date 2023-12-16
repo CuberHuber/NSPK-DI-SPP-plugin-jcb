5953761cd36688791aa4909cc04e7438cfb0fccb
CuberHuber <lupashcko.rom@yandex.ru>
2023-12-16 14:48:57 +0500

    Изменил парсер для более быстрой работы.

---
+++ jcb.test.py
@@ -0,0 +1,107 @@
+"""
+Нагрузка плагина SPP
+
+1/2 документ плагина
+"""
+import datetime
+import logging
+import os
+import pickle
+import re
+import time
+
+import dateutil.parser
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.common import NoSuchElementException
+
+from src.spp.types import SPP_document
+
+
+class JCB:
+    """
+    Класс парсера плагина SPP
+
+    :warning Все необходимое для работы парсера должно находится внутри этого класса
+
+    :_content_document: Это список объектов документа. При старте класса этот список должен обнулиться,
+                        а затем по мере обработки источника - заполняться.
+
+
+    """
+
+    REF_NAME = 'jcb'
+    HOST = 'https://www.global.jcb/en/press/index.html'
+
+    YEAR_BEGIN = 2023
+    HOME_URL = 'https://www.global.jcb/en/press/index.html'
+    TEMPLATE_URL = 'https://www.global.jcb/en/press/index.html?year={year}'
+    _content_document: list[SPP_document]
+
+    def __init__(self, webdriver: WebDriver, *args, **kwargs):
+        """
+        Конструктор класса парсера
+
+        По умолчанию внего ничего не передается, но если требуется (например: driver селениума), то нужно будет
+        заполнить конфигурацию
+        """
+        # Обнуление списка
+        self._content_document = []
+        self.driver = webdriver
+
+        # Логер должен подключаться так. Вся настройка лежит на платформе
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.debug(f"Parser class init completed")
+        self.logger.info(f"Set source: {self.REF_NAME}")
+        ...
+
+    def content(self) -> list[SPP_document]:
+        """
+        Главный метод парсера. Его будет вызывать платформа. Он вызывает метод _parse и возвращает список документов
+        :return:
+        :rtype:
+        """
+        self.logger.debug("Parse process start")
+        self._parse()
+        self.logger.debug("Parse process done")
+        return self._content_document
+
+    def _parse(self):
+        """
+        Метод, занимающийся парсингом. Он добавляет в _content_document документы, которые получилось обработать
+        :return:
+        :rtype:
+        """
+        # HOST - это главная ссылка на источник, по которому будет "бегать" парсер
+        self.logger.debug(F"Parser enter to {self.HOST}")
+
+        # ========================================
+        # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
+        # -
+        try:
+            with open('backup/documents.backup.pkl', 'wb') as file:
+                docs = pickle.load(file)
+                for doc in docs:
+                    self._content_document.append(doc)
+        except Exception as e:
+            print(e)
+
+        # Логирование найденного документа
+        # self.logger.info(self._find_document_text_for_logger(document))
+
+        # ---
+        # ========================================
+        ...
+
+    @staticmethod
+    def _find_document_text_for_logger(doc: SPP_document):
+        """
+        Единый для всех парсеров метод, который подготовит на основе SPP_document строку для логера
+        :param doc: Документ, полученный парсером во время своей работы
+        :type doc:
+        :return: Строка для логера на основе документа
+        :rtype:
+        """
+        return f"Find document | name: {doc.title} | link to web: {doc.web_link} | publication date: {doc.pub_date}"
